6c292280377ec8da8d9c5821fe8645c951d96694
Diego Nobre <sounobre@hotmail.com>
2020-11-03 13:20:00 -0300

    estilo global e inicio do signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import './App.css';
+import SignIn from './pages/Signin';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Hello World</h1>;
-
-
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 export default App;
